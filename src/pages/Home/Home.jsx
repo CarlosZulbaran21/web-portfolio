7a9c7a2c0104eddd0941e825e4bcc66568bab142
Carlos Zulbaran <czulbaran@supre.com.co>
2023-04-06 15:44:06 -0500

    styling header

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,29 @@
-import Header from "../../components/Header/Header";
+import "./Home.css";
+import SvgOrange from "../../assets/svg/SvgOrange";
+import SvgCoveragePhoto from "../../assets/svg/SvgCoveragePhoto";
+import Img1 from "../../assets/images/1.jpeg";
+import Person from "../../components/Person/Person";
 
 export default function Home() {
   return (
-    <>
-      <Header />
-    </>
+    // <section className="section">
+    //   <div style={{
+    //     padding : '1rem',
+    //     maxWidth: '28rem',
+    //     width: '100%'
+    //   }}>
+        
+    //   </div>
+    //   <article className="section__article article">
+    //     <div className="article__svg-container">
+    //       <SvgOrange />
+    //       <SvgCoveragePhoto />
+    //       <img src={Img1} alt="imagen" height={300}style={{position: 'absolute', top:0}}/>
+    //     </div>
+    //   </article>
+
+      
+    // </section>
+    <Person/>
   );
 }
