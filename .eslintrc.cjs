90beb916774a0670e784b7bc8450638b0ca87363
Carlos Zulbaran <czulbaran@supre.com.co>
2023-04-05 19:52:12 -0500

    feat: prettier configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,16 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard'
-  ],
-  overrides: [
-  ],
+  extends: ["plugin:react/recommended", "standard", "prettier-config-standard"],
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    'react'
-  ],
+  plugins: ["react"],
   rules: {
-    'react/react-in-jsx-scope': 'off'
-  }
-}
+    "react/react-in-jsx-scope": "off",
+  },
+};
